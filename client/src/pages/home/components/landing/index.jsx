29e59912cf34f777a33e75a1bccc35688368977d
xander <shuaibualexander@gmail.com>
2021-11-29 11:14:14 +0100

    create cooperative

--- client/src/pages/home/components/landing/index.jsx
+++ client/src/pages/home/components/landing/index.jsx
@@ -1,25 +1,29 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import BgCream from '../../../../assets/small-landingbackground.png'
+import BgCream from "../../../../assets/small-landingbackground.png";
 
 import LeftArrow from "../../../../assets/leftarrow.svg";
 import styles from "./landing.module.scss";
 
-
-
 export default function Index() {
-
-  const [state, handleSubmit] = useState('');
-  const submittedStyle = state.succeeded ? { visibility: 'hidden' } : {};
-  const submittedText = state.succeeded ? "Thank you! we will be in touch" : "Get started by creating your own coopoerative";
+  const [state, handleSubmit] = useState("");
+  const submittedStyle = state.succeeded ? { visibility: "hidden" } : {};
+  const submittedText = state.succeeded
+    ? "Thank you! we will be in touch"
+    : "Get started by creating your own cooperative";
 
   return (
     <section className={styles.landing__cover}>
       <div className={styles.landing__content}>
         <h1> Our Aim? </h1>
         <h4>
-          We aim to create a peer to peer financial lending protocol based on the age old tradition called <i>Ajo</i>',
-          which presents a round robin style of claiming a pool of funds provided by peers.
+          We aim to create a peer to peer financial lending protocol based on
+          the age old Nigerian tradition called{" "}
+          <a href="https://www.pulse.ng/lifestyle/money/saving-scheme-ajo-benefits-of-contributory-thrift-saving-scheme/2sfb1ne">
+            <i>Ajo</i>
+          </a>
+          , which presents a round robin style of claiming a pool of funds
+          provided by peers.
         </h4>
         <h3>{submittedText}</h3>
         <form
@@ -37,7 +41,8 @@ export default function Index() {
         </form>
       </div>
       <div className="--smallshow">
-        <img alt="nice cream" src={BgCream} />
+        <div style={{width: "100%", height:"5vh"}}>
+        </div>
       </div>
     </section>
   );
