8d7a60e6eed09923b4e53e157ba956fd16f222d2
xander <shuaibualexander@gmail.com>
2021-11-30 03:50:30 +0100

    build files

---
+++ client/build/precache-manifest.b47af73a9998e17cbdbb1a73f772a3dd.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7080f07312711d25fabb006a6c91e5a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1165cc795f83582c67b0",
+    "url": "/static/css/2.deb131e7.chunk.css"
+  },
+  {
+    "revision": "25bca448c2b9cbf8d8a2",
+    "url": "/static/css/main.ab02a89e.chunk.css"
+  },
+  {
+    "revision": "1165cc795f83582c67b0",
+    "url": "/static/js/2.8ff03d65.chunk.js"
+  },
+  {
+    "revision": "25bca448c2b9cbf8d8a2",
+    "url": "/static/js/main.61269ee0.chunk.js"
+  },
+  {
+    "revision": "92c89455b99cd109be4a",
+    "url": "/static/js/runtime-main.b5d1efea.js"
+  },
+  {
+    "revision": "07909c0eae1eac50bd0fa3a8ac21270c",
+    "url": "/static/media/MabryPro-Regular.07909c0e.ttf"
+  },
+  {
+    "revision": "9846afbba073dcee4f5d99e066a76ffc",
+    "url": "/static/media/female.9846afbb.png"
+  },
+  {
+    "revision": "0cb35dd4b4c7b6d36c77224fdd944524",
+    "url": "/static/media/group.0cb35dd4.svg"
+  },
+  {
+    "revision": "652fac1cc244c3433a52f2c4f2c9485e",
+    "url": "/static/media/logowhite.652fac1c.svg"
+  },
+  {
+    "revision": "f7df46fa3a2a8dadd5a26cddfd9a1674",
+    "url": "/static/media/lost.f7df46fa.svg"
+  },
+  {
+    "revision": "c879a582a94e772b8ed27295ae83145d",
+    "url": "/static/media/metamask.c879a582.svg"
+  },
+  {
+    "revision": "7f642992c194c4dfe60e8da0b1012331",
+    "url": "/static/media/plainbg.7f642992.png"
+  }
+]);
